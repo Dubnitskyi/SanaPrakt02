427ff86c193d26d129eb0599eeee1d8fc9dde5ba
Yaroslav Dubnitskyi <ipz214_dyav@student.ztu.edu.ua>
2022-11-22 14:05:11 +0200

    TEXT Commit 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Sana Commerce ");
+Console.WriteLine("Sana Commerce ");
+Console.WriteLine("Sana Commerce ");
 Console.WriteLine("Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Sana Commerce Software Development Bootcamp");
